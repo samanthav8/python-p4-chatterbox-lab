ef7bcb74d3194908094406e560c2b7b65e43237f
Samantha <samanthavillafana8@gmail.com>
2024-10-21 22:47:01 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,13 +13,42 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+# Get all messages
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at).all()
+    return jsonify([message.to_dict() for message in messages]), 200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+# Create a new message
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
+
+# Update a message
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = db.session.get(Message, id)
+    if message:
+        body = request.json.get('body')
+        if body:
+            message.body = body
+            db.session.commit()
+            return jsonify(message.to_dict()), 200
+    return {"error": "Message not found"}, 404
+
+# Delete a message
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = db.session.get(Message, id)
+    if message:
+        db.session.delete(message)
+        db.session.commit()
+        return {}, 204
+    return {"error": "Message not found"}, 404
 
 if __name__ == '__main__':
     app.run(port=5555)
